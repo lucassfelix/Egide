af213d4eb6b580ea24993e841a2dd3b720761359
lucassfelix <lsfelix321@gmail.com>
2020-10-20 11:15:56 -0300

    Horizontal Movement Accelaration

--- Egide/Assets/Script/TestMovementeScript.cs
+++ Egide/Assets/Script/TestMovementeScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class TestMovementeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private float jumpStrength = 5;
+
+    [SerializeField]
+    private float accelerationRate = 1f;
+    [SerializeField]
+    private float deaccelerationRate = 1f;
+
+    private Rigidbody2D rb;
+    private float axis;
+    private float lerpValue = 0.5f;
+
     void Start()
     {
-        
+        this.rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.D))
+        {
+            lerpValue = Mathf.MoveTowards(lerpValue, 1f ,accelerationRate * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            lerpValue = Mathf.MoveTowards(lerpValue, 0f ,accelerationRate * Time.deltaTime);
+        }
+        else
+        {
+            lerpValue = Mathf.MoveTowards(lerpValue, 0.5f ,deaccelerationRate * Time.deltaTime);
+        }
+
+        axis = Mathf.Lerp(-1,1,lerpValue);
     }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(maxSpeed * axis,rb.velocity.y);
+    }
+
 }
